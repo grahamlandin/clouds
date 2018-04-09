bc0bbb14340cbba020a779d08ee96ac55c3d0784
Ian Edmonds <ianladneredmonds@gmail.com>
2018-04-08 19:06:07 -0700

    add simple express app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const PORT = 1337
+
+app.use(express.static('assets'))
+
+app.get('/', (req, res) => { 
+  res.sendFile(path.join(__dirname + '/assets/clouds/clouds.html'))
+})
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
